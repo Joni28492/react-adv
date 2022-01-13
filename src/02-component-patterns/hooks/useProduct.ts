977ebc2cf342dab2d07e074d99e4e025331204eb
Joni Fernández <joni28492@gmail.com>
2022-01-13 13:12:45 +0100

    control props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductArgs {
@@ -13,14 +13,10 @@ export const useProduct = ({onChange, product, value=0}:useProductArgs) => {
 
     const [counter, setCounter] = useState(value);
 
-    const isControlled = useRef( !!onChange )//bool
+    
 
     const increaseBy = (value: number) =>{
-        if(isControlled.current){
-            return onChange!({count: value, product})
-        }
-
-
+    
         const newValue =  Math.max(counter + value, 0 )
         setCounter(newValue);
         onChange &&  onChange({count: newValue, product});
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -7,37 +7,24 @@ import { Product, ProductInCart } from "../interfaces/interfaces";
 export const useShoppingCart = () => {
 
     const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
-    // console.log(shoppingCart);
-
+    
     const onProdcutCardChange = ({ count, product }: { count: number, product: Product }) => {
+        console.log({count});
 
 
 
         setShoppingCart(oldShoppingCart => {
 
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count }
 
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
+            if (count === 0) {
+            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+            return rest;
             }
 
-            //borrar el producto si esta a 0
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return { ...rest };//valdria tambien solo
-
-            // if (count === 0) {
-            // const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            // return rest;
-            // }
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [product.id]: { ...product, count }
-            // }
+            return {
+                ...oldShoppingCart,
+                [product.id]: { ...product, count }
+            }
         })
     }
 
